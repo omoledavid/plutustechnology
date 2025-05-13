dad165c6c5d33173b8c5f889c3c867f9cc829d7b
Omole kessiena David <edokadavid@gmail.com>
2025-05-13 03:11:06 +0100

    update nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,18 +1,18 @@
 <li class="{{setActiveMenu('home')}}">
-    <a href="{{route('home')}}">Home</a><i class="bi bi-plus dropdown-icon"></i>
+    <a href="{{route('home')}}">Home</a><i></i>
 </li>
 <li class="{{setActiveMenu( 'about')}}">
-    <a href="{{route('about')}}" class="drop-down">About Us</a><i class="bi bi-plus dropdown-icon"></i>
+    <a href="{{route('about')}}">About Us</a><i></i>
 </li>
 <li class="{{setActiveMenu( 'services')}}">
-    <a href="{{route('services')}}" class="drop-down">Services</a><i class="bi bi-plus dropdown-icon"></i>
+    <a href="{{route('services')}}">Services</a><i></i>
 </li>
 <li class="{{setActiveMenu( 'projects')}}">
-    <a href="{{route('projects')}}" class="drop-down">Projects</a><i class="bi bi-plus dropdown-icon"></i>
+    <a href="{{route('projects')}}">Projects</a><i></i>
 </li>
 <li class="{{setActiveMenu( 'blog')}}">
-    <a href="{{route('blog')}}" class="drop-down">Blog</a><i class="bi bi-plus dropdown-icon"></i>
+    <a href="{{route('blog')}}">Blog</a><i></i>
 </li>
 <li class="{{setActiveMenu( 'contact')}}">
-    <a href="{{route('contact')}}" class="drop-down">Contact Us</a>
+    <a href="{{route('contact')}}">Contact Us</a>
 </li>
